af121d44a681baf67d68e4f393f530f5f80f57cf
Eldius <eldiosantos@gmail.com>
2017-02-08 16:59:34 -0200

    Adding tests to test...

---
+++ dropbox-api/src/test/java/net/eldiosantos/cloudstorage/dropbox/service/RunCukesTest.java
@@ -0,0 +1,15 @@
+package net.eldiosantos.cloudstorage.dropbox.service;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by esjunior on 08/02/2017.
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    plugin = {"pretty", "html:build/cucumber"}
+)
+public class RunCukesTest {
+}
---
+++ dropbox-api/src/test/resources/net/eldiosantos/cloudstorage/dropbox/service/test.feature
@@ -0,0 +1,7 @@
+
+Feature: Test the cucumber library
+  To test the library usage with generic steps
+
+  Scenario: Execute my test steps
+    Given the following animals: cow, horse, sheep
+    Then I have 10 cukes in my belly
